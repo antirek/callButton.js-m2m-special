ab5dfd7d386aa3086e7ac75aa620385772130f01
antirek <serge.dmitriev@gmail.com>
2015-06-09 14:53:49 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # callButton.js-m2m-special
 callButton.js freeze for m2m (special)
+
+concat callButton.js and init.js to one file in ./dist
+use gulp
+
---
+++ init.js
@@ -0,0 +1,10 @@
+callButton("u2d6embblq", { 
+    title: "Перезвонить Вам?", 
+    titleColor: "#FFFFFF", 
+    backgroundColor: "#104E8B", 
+    top: "150px", 
+    icon: false, 
+    iconAnimated: false, 
+    intrusiveMode: false, 
+    intrusiveTimeout: 30 
+});
